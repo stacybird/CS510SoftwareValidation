81e49c1b1036d69765c0b63cad2b15c4d53a6a8d
stacybird <dreamsong@gmail.com>
2015-04-30 21:53:44 -0700

    base tests file with first failing test

---
+++ TicTacToe/tests/TicTacToeTest.java
@@ -0,0 +1,27 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by stacy on 4/30/15.
+ */
+public class TicTacToeTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testMain() throws Exception {
+        assertTrue(false);
+
+    }
+}

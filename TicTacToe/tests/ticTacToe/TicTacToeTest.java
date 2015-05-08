39f2ba58d4f8b4255b5b8b304d91545af3d0049e
stacybird <dreamsong@gmail.com>
2015-05-08 00:25:35 -0700

    try an output stream approach, currently hangs

--- TicTacToe/tests/ticTacToe/TicTacToeTest.java
+++ TicTacToe/tests/ticTacToe/TicTacToeTest.java
@@ -2,27 +2,40 @@ package ticTacToe;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by stacy on 4/30/15.
+ * This is an example on testing the public contract
  */
 public class TicTacToeTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
     @Before
     public void setUp() throws Exception {
-
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
     }
 
     @After
     public void tearDown() throws Exception {
-
+        System.setOut(null);
+        System.setErr(null);
     }
 
     @Test
     public void testMain() throws Exception {
-        assertTrue(false);
+        String expected = "Below is the board coordinate system:";
+        String[] arguments = new String [0];
+        TicTacToe.main(arguments);
+        assertEquals(expected, outContent.toString());
 
     }
+
+
+
 }

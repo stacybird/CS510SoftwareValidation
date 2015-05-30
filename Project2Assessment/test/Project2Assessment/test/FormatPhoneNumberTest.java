bffbe5387de60d5b5078703c08ef01194b5d5572
stacybird <dreamsong@gmail.com>
2015-05-30 14:18:52 -0700

    add notes from instructor as header

--- Project2Assessment/test/Project2Assessment/test/FormatPhoneNumberTest.java
+++ Project2Assessment/test/Project2Assessment/test/FormatPhoneNumberTest.java
@@ -8,6 +8,18 @@ import static org.junit.Assert.*;
 
 /**
  * Created by stacy on 5/30/15.
+ *
+ * Notes from instructor:
+ *
+ * Write the bodies for the following test methods, plus any additional test
+ * methods that would be appropriate.  Use helper methods and inheritance
+ * where it makes sense (assertions should never go anywhere but a test
+ * method body).  Regardless of the test result, an appropriate message should
+ * print to the console when:
+ *  -- Testing of this class begins,
+ *  -- Any particular test begins,
+ *  -- Any particular test ends, and
+ *  -- Test of this class ends.
  */
 public class FormatPhoneNumberTest {
 

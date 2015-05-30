ff761eea285be7dc63fd4c8f60edfb2241621ae6
stacybird <dreamsong@gmail.com>
2015-05-30 14:05:30 -0700

    junit empty shell for test cases, first set she requested for the demo

---
+++ Project2Assessment/test/Project2Assessment/test/FormatPhoneNumberTest.java
@@ -0,0 +1,41 @@
+package Project2Assessment.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by stacy on 5/30/15.
+ */
+public class FormatPhoneNumberTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void sevenDigitPhoneNumberWithoutExtension() throws Exception {
+
+    }
+
+    @Test
+    public void tenDigitPhoneNumberWithoutExtension() throws Exception {
+
+    }
+    @Test
+    public void elevenDigitPhoneNumberWithoutExtension() throws Exception {
+
+    }
+    @Test
+    public void elevenDigitPhoneNumberWithExtension() throws Exception {
+
+    }
+}
